b3d63ad02276c27e2766c50a2b3aec01ac6f6e6d
syth09 <tatuanphong616@gmail.com>
2024-09-06 22:28:22 +0700

    Naming dynamicly

--- p2-training/src/Message.tsx
+++ p2-training/src/Message.tsx
@@ -2,7 +2,8 @@
 function Message() {
   //JSX: Javascript XML
   const name = "JunFeng";
-  return <h1>Hello {name}</h1>;
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
 }
 
 export default Message;
