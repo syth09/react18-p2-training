ab414e58a4e1a9fe4fcf32ed0bb86f1b922fa04a
syth09 <tatuanphong616@gmail.com>
2024-09-06 22:29:01 +0700

    Naming dymanicly

--- p2-training/src/Message.tsx
+++ p2-training/src/Message.tsx
@@ -1,7 +1,7 @@
 // PascalCasing
 function Message() {
   //JSX: Javascript XML
-  const name = "JunFeng";
+  const name = "";
   if (name) return <h1>Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
