688994fb186989b8f183b97ae12f3266ce051668
syth09 <tatuanphong616@gmail.com>
2024-09-08 16:37:51 +0700

    Conditional rendering

--- p2-training/src/components/ListGroup.tsx
+++ p2-training/src/components/ListGroup.tsx
@@ -1,9 +1,19 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // Stored the condition in a separate variables or constants
+  //   const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // Stored it inside a function
+  //   const getMessage = () => {
+  //     items.length === 0 ? <p>No item found</p> : null;
+  //   };
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
