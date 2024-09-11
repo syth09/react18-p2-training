ec3138cfaae029769edc37ed3fc81523f1bd8415
syth09 <tatuanphong616@gmail.com>
2024-09-11 16:31:44 +0700

    Passing Data via Props

--- p2-training/src/App.tsx
+++ p2-training/src/App.tsx
@@ -1,7 +1,7 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <div>
