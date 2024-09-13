4fb89deaf64c4eaf0acca99d15f0a982a858d00a
syth09 <tatuanphong616@gmail.com>
2024-09-13 22:41:10 +0700

    Updating a Deeply Nested Objects

--- p2-training/src/App.tsx
+++ p2-training/src/App.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111
+    }
   });
 
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 }
+    });
   };
 
   return (
     <div>
-      {drink.price}
       <button onClick={handleClick}>Click me</button>
     </div>
   );
