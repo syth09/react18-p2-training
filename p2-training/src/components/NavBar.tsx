c3540c9221b9fd6d7b25ccb25359e75cf62ecd4e
syth09 <tatuanphong616@gmail.com>
2024-09-14 20:43:54 +0700

    Sharing State between Components

---
+++ p2-training/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+interface NavBarProps {
+  cartItemsCount: number;
+}
+
+const NavBar = ({ cartItemsCount }: NavBarProps) => {
+  return <div>NavBar: {cartItemsCount}</div>;
+};
+
+export default NavBar;
