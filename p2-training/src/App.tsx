4068868a5be4dd5b8e7b95cc0a6b2e3ee0db6918
syth09 <tatuanphong616@gmail.com>
2024-09-16 14:55:40 +0700

    Effect dependencies e.g

--- p2-training/src/App.tsx
+++ p2-training/src/App.tsx
@@ -1,9 +1,20 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [category, setCategory] = useState("");
+
   return (
     <div>
-      <Form />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
